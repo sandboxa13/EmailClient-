9376998f492c387cc88eb2d98f95fb207dc398e0
Dmitry <dima_nikishin_84@mail.ru>
2020-03-23 19:19:11 +0300

    update new message view

--- src/EmailClient/EmailClient/ViewModels/NewMessageViewModel.cs
+++ src/EmailClient/EmailClient/ViewModels/NewMessageViewModel.cs
@@ -4,12 +4,14 @@ using System.Reactive.Linq;
 using System.Windows.Input;
 using EmailClient.Managers;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace EmailClient.ViewModels
 {
     public class NewMessageViewModel :BaseViewModel
     {
         private readonly ReactiveCommand<Unit, Unit> _sendMessage;
+        private readonly ReactiveCommand<Unit, Unit> _attach;
 
         public NewMessageViewModel(
             INavigationManager navigationManager, 
@@ -17,11 +19,17 @@ namespace EmailClient.ViewModels
             : base("NewMessageViewModel")
         {
             _sendMessage = ReactiveCommand.CreateFromTask(mailKitApiManager.SendMessageAsync);
+            _attach = ReactiveCommand.CreateFromTask(mailKitApiManager.SendMessageAsync);
 
             _sendMessage.Select(unit => typeof(MainPageViewModel))
                 .Subscribe(navigationManager.Navigate);
         }
 
         public ICommand SendMessageCommand => _sendMessage;
+        public ICommand AttachCommand => _attach;
+
+        [Reactive] public string From { get; private set; }
+        [Reactive] public string To { get; private set; }
+        [Reactive] public string Message { get; private set; }
     }
 }
